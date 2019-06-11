4155e4e26a23302ea9598dd96a59dcd09415f20f
Ruichen <huangruichen0226@gmail.com>
2019-06-11 11:17:52 -0400

    Failing version

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.get('/intro', function(req, res, next) {
+  res.render('intro');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
